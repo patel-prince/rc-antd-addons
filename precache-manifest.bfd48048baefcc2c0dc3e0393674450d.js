16f905f9fe84a5ee9f77d661bf4048a53d74034f
Prince <prpatel0949@gmail.com>
2021-09-13 17:06:03 +0530

    Updates

---
+++ precache-manifest.bfd48048baefcc2c0dc3e0393674450d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8b81dcd18871eedf3ca854b6549062",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ecc78007c3793512a890",
+    "url": "./static/css/2.486ebae0.chunk.css"
+  },
+  {
+    "revision": "bd8f66a6102eb352278a",
+    "url": "./static/css/main.dd8ba41d.chunk.css"
+  },
+  {
+    "revision": "ecc78007c3793512a890",
+    "url": "./static/js/2.843d1975.chunk.js"
+  },
+  {
+    "revision": "41e8aedee29e6e7a9a1bdd0a210ba283",
+    "url": "./static/js/2.843d1975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8f66a6102eb352278a",
+    "url": "./static/js/main.4bf42d79.chunk.js"
+  },
+  {
+    "revision": "045d4eee148c8b590b0b",
+    "url": "./static/js/runtime-main.b8b621cd.js"
+  }
+]);
